987d7ad9d632be02ed76e8afe72d2f0506a24eaf
rishilahoti <rishilahoti99@gmail.com>
2024-09-12 17:44:22 +0530

    initial commit with theme

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	darkMode: ['class'],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+		'./app/**/*.{js,ts,jsx,tsx}', // Add the app directory
+	],
+	theme: {
+		extend: {
+			borderRadius: {
+				lg: 'var(--radius)',
+				md: 'calc(var(--radius) - 2px)',
+				sm: 'calc(var(--radius) - 4px)',
+			},
+			colors: {},
+		},
+	},
+	plugins: [require('tailwindcss-animate')],
+};
